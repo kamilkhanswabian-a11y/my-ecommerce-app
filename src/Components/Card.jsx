4de02e7e6dcfc3bfeb2eb5316d04d9b169083baf
Kamil khan <kamilkhanswabian@gmail.com>
2026-08-15 03:19:32 +0500

    Card Component Polished

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,28 +2,28 @@ import { Star, Heart, HeartIcon } from 'lucide-react';
 import { useContext } from 'react';
 import { CartContext } from '../Context/Cartcontext';
 import { Wishlistcontext } from '../Context/Whislistcontext';
-import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router';
 import Cards from './Cards';
 import Button from './Button';
 function Card({ data }) {
   const { addToCart } = useContext(CartContext);
   const { addToWishlist, isInWishlist } = useContext(Wishlistcontext);
-  const navigate = useNavigate();
   return (
           <li>
                {data.length === 0 && (
                    <h1 className='flex justify-center items-center text-black '>No product Founds...</h1>
                )}
                
-               <Cards className='flex flex-col gap-3 bg-gray-200 border border-gray-100 rounded-md relative'>
+               <Cards className='flex flex-col gap-3 bg-gray-200 border border-gray-100 rounded-2xl relative'>
                              
                             <div className='flex absolute right-2 top-2 text-white'>
                           <HeartIcon onClick={()=> addToWishlist(data)}
                               className={`w-6 h-6 cursor-pointer ${isInWishlist(data.id) ? ' fill-red-600' : 'bg-transparent text-white'}`}
                                 />
                             </div>
-    
-                          <img src={data.images[0]} alt="" className='h-40 object-cover overflow-hidden p-1'/>
+                          <div>
+                              <Link to={`/Detail/${data.id}`} className='flex flex-col gap-4'>
+                          <img src={data.images[0]}  className='h-40 object-cover overflow-hidden rounded-md'/>
                           <p className='font-serif text-black ml-2'>{data.name}</p>
     
                           <div  className='flex justify-between px-1'>
@@ -39,7 +39,9 @@ function Card({ data }) {
                                           {data.rating}
                                    </p>
                           </div>
-                          <Button className='bg-black/50 text-white p-2 rounded-lg mb-1 mx-5'
+                              </Link>
+                          </div>
+                          <Button className='bg-black/50 text-white p-2 rounded-xl my-3 mx-5 '
                            onClick={()=> addToCart(data)}
                           >Add To Cart</Button>
                </Cards>
