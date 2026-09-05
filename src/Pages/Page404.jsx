de8a0af2a4fbee70d646162bdcde15470dde7eaf
Kamil khan <kamilkhanswabian@gmail.com>
2026-09-05 10:24:02 +0500

    add:  404 page add

---
+++ src/Pages/Page404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page404() {
+  return (
+    <div className='h-screen text-4xl flex items-center justify-center font-semibold text-gray-800'>Page Not Find 404</div>
+  )
+}
+
+export default Page404
